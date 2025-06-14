fea61f5ccfd086e62f291d4feff63ebf46d77891
Calvin Post <calvinator08@gmail.com>
2025-06-14 13:50:32 -0500

    Installed dart-sass.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
-  return (
-    <div className="App">Hello Everyone!</div>
-  )
-}
+  return <div className="App">Hello Everyone!</div>;
+};
 
-export default App
+export default App;
